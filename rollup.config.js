df640cd9cc05434b20533b564902ee54ab3cd949
Simon Bechmann <sbechmann@hotmail.com>
2022-05-17 22:03:46 +0200

    Added packages and configs to build and maintain project

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+	input: 'src/main.ts',
+	output: {
+		dir: 'dist',
+		format: 'cjs'
+	},
+	plugins: [
+		typescript({
+			tsconfig: './tsconfig.json'
+		})
+	]
+};
