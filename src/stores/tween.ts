255ef35e7bbe45df35315de0bb3bbe1850d0061e
Simon Bechmann <sbechmann@hotmail.com>
2022-05-28 21:06:41 +0200

    Fix: Not calling stop in constructor through validate

--- src/stores/tween.ts
+++ src/stores/tween.ts
@@ -6,33 +6,42 @@ export class Tween extends Store<number> {
 	public duration: number;
 	public easeMethod: EaseMethod;
 	private readonly _timer: FrameTimer;
+  private _tweening: boolean;
 
 	constructor(value: number = 0, duration: number = 1000, easeMethod: EaseMethod = cubInOut) {
 		super(value);
 		this.duration = duration;
 		this.easeMethod = easeMethod;
 		this._timer = new FrameTimer();
+    this._tweening = false;
 	}
 
 	protected validate(newValue: number): number {
-		this.stop();
+		if (this._tweening) {
+      this.stop();
+    }
 		return newValue;
 	}
 
 	public start(targetValue: number, onDone?: () => void): void {
 		this.stop();
+    this._tweening = true;
 		const initialValue: number = this._value;
 		this._timer.start(this.duration, (progress: number) => {
 			const t: number = progress / this.duration;
 			this.value = interpolate(initialValue, targetValue, this.easeMethod, t);
 
-			if (t === 1 && onDone != null) {
-				onDone();
+			if (t === 1) {
+        this._tweening = false;
+        if (onDone != null) {
+          onDone();
+        }
 			}
 		});
 	}
 
 	public stop(): void {
 		this._timer.stop();
+    this._tweening = false;
 	}
 }
